8d11ae9afaac07029bcd919c1fa5655e34aefce6
uliana2006eskova <uliana.eskova@gmail.com>
2020-02-29 12:38:14 +0300

    change settings

--- metro/metro/settings.py
+++ metro/metro/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'questionrequest.apps.QuestionrequestConfig',
+    'authorization.apps.AuthorizationConfig',
 ]
 
 MIDDLEWARE = [
